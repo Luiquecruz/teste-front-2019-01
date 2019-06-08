07dabcfd70ba5f18a080972cd2a7a746f78176e5
Luique <luiquecruz@gmail.com>
2019-06-08 12:43:48 -0300

    new actions added

--- swui/src/App.vue
+++ swui/src/App.vue
@@ -13,8 +13,47 @@
       <ul class="light--bg">
         <li class="dark" v-for="specie in species" :key="specie.created">
           <p>
+            <strong class="d-block">
+              Specie Name
+            </strong>
+
             {{ specie.name }}
           </p>
+
+          <p>
+            <strong class="d-block">
+              Language
+            </strong>
+
+            {{ specie.language }}
+          </p>
+
+          <p>
+            <strong class="d-block">
+              Homeworld
+            </strong>
+          </p>
+
+          <ul class="white--bg">
+            <li v-for="planet in planets" :key="planet.created">
+              {{ planet.name }}
+            </li>
+          </ul>
+
+          <p>
+            <strong>
+              People
+            </strong>
+          </p>
+
+          <ul class="white--bg">
+            <li v-for="person in people" :key="person.created">
+              {{ person.name }}
+            </li>
+          </ul>
+
+          <br>
+          <hr>
         </li>
       </ul>
     </div> <!-- end container -->
@@ -32,6 +71,8 @@ import { mapState, mapActions } from 'vuex'
 export default {
   mounted () {
     this.listSpecies()
+    this.listPlanets()
+    this.listPeople()
   },
   components: {
     Header,
@@ -39,12 +80,16 @@ export default {
   },
   computed: {
     ...mapState([
-      'species'
+      'species',
+      'planets',
+      'people'
     ])
   },
   methods: {
     ...mapActions([
-      'listSpecies'
+      'listSpecies',
+      'listPlanets',
+      'listPeople',
     ])
   }
 }
@@ -52,5 +97,4 @@ export default {
 
 <style lang="scss">
   @import "./assets/sass/global";
-
 </style>
--- swui/src/store.js
+++ swui/src/store.js
@@ -7,11 +7,19 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    species: []
+    species: [],
+    planets: [],
+    people: []
   },
   mutations: {
     setSpecie (state, species) {
       state.species = species
+    },
+    setPlanet (state, planets) {
+      state.planets = planets
+    },
+    setPeople (state, people) {
+      state.people = people
     }
   },
   actions: {
@@ -21,6 +29,28 @@ export default new Vuex.Store({
         .then(response => {
           const speciesList = response.data.results
           commit('setSpecie', speciesList)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    listPlanets ({ commit }) {
+      http
+        .get('planets/')
+        .then(response => {
+          const planetsList = response.data.results
+          commit('setSpecie', planetsList)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    listPeople ({ commit }) {
+      http
+        .get('people/')
+        .then(response => {
+          const peopleList = response.data.results
+          commit('setPeople', peopleList)
         })
         .catch(error => {
           console.log(error)
